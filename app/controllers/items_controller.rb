c38bb49da4b809a31e2d01fde4923d975a8cb5f3
satoba-30 <august03alone@gmail.com>
2021-04-05 16:36:56 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username,password|
-      username == ENV["BASIC_AUTH_USE"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
     
   end
