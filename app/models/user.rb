5c01b32243c7068041556babf89676ff08a97e84
satoba-30 <august03alone@gmail.com>
2021-04-08 21:32:11 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,22 @@ class User < ApplicationRecord
          
   validates :nickname,presence: true
   
-  validates_format_of :password, with: VALID_PASSWORD_REGEX,message: "Password Include both letters and numbers" 
+  validates_format_of :password, with: VALID_PASSWORD_REGEX,message: " include both letters and numbers" 
 
   validates :last_name,presence: true,
-            format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
-            message: "Last name can't be blank" }
+            format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+            
 
   validates :first_name,presence: true,
-            format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
-            message: "First name can't be blank"}
+            format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
 
   validates :last_name_kana,presence: true,
             format: {with: /\A[ァ-ヶー－]+\z/,
-            message: "Last name kana Full-width katakana characters" }
+            message: " Full-width katakana characters" }
 
   validates :first_name_kana,presence: true,
             format: {with: /\A[ァ-ヶー－]+\z/,
-            message: "First name kana Full-width katakana characters"}
+            message: " Full-width katakana characters"}
             
   validates :birthday,presence: true
 end
