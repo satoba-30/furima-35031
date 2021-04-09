8852b1b2a230871e3972ae569df2d581bf7455e4
satoba-30 <august03alone@gmail.com>
2021-04-09 19:44:13 +0900

    itemモデルにバリデーションを設けた

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  
+  
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :category_id
+    validates :text
+    validates :status_id
+    validates :delivery_fee_id
+    validates :post_address_id
+    validates :post_daytime_id 
+  end
 end
