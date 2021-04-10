cdfaaf6b328370b1b593b2e80a820a8d32186d7f
satoba-30 <august03alone@gmail.com>
2021-04-10 17:34:01 +0900

    バリデーションを修正１、itemsコントローラーにparamsを記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,17 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @item = Item.new(item_params)
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+  def item_params 
+    params.require(:item).permit(:name,:price,:text,:category_id,:status_id,:delivery_fee_id,:post_address_id,:post_daytime_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,19 @@
 class Item < ApplicationRecord
-  belongs_to :user
   has_one_attached :image
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  has_one :status
+  has_one :delivery_fee
+  belongs_to :post_address
+  belongs_to :post_daytime
 
+  
+  validates :price, numericality: {greater_than: 300,less_than: 9999999}
+  
   with_options presence: true do
-    validates :name
-    validates :price, format: { with: /\A[0-9]+\z/ }
-    validates :text
+    validates :name, {max_length: 40}
+    validates :text, {max_length: 1000}
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :status_id
