35366f9a62ff31cd8ea2dee341faed636acaf5de
satoba-30 <august03alone@gmail.com>
2021-04-12 16:43:29 +0900

    itemの出品実装、デーブルに保存可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,6 +24,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params 
-    params.require(:item).permit(:name,:price,:text,:category_id,:status_id,:delivery_fee_id,:post_address_id,:post_daytime_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:price,:text,:image,:category_id,:status_id,:delivery_fee_id,:post_address_id,:post_daytime_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   has_one :status
@@ -9,11 +10,13 @@ class Item < ApplicationRecord
   belongs_to :post_daytime
 
   
-  validates :price, numericality: {greater_than: 300,less_than: 9999999}
+  validates :price,presence: true,format: { with: /\A[0-9]+\z/},
+                   numericality: {greater_than: 300,less_than: 9999999}
   
   with_options presence: true do
-    validates :name, {max_length: 40}
-    validates :text, {max_length: 1000}
+    validates :name, length: { maximum: 40 }      
+    validates :text, length: { maximum: 1000 }
+    validates :image      
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :status_id
